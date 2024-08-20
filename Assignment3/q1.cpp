f61fd82e4bcd469c73b990c2def752107dfc47fb
maryambigonah <129228080+maryambigonah@users.noreply.github.com>
2024-08-20 09:24:31 -0500

    Add files via upload

---
+++ Assignment3/q1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	string first, second;
+
+	int added[20];
+    
+	int carry = 0;
+	// Get the values
+	cout << "Enter the first number:";
+	cin  >> first;
+	cout << "Enter the second number:";
+	cin  >> second;	
+
+	// Convert digits to ints
+	int x[first.length()], y[second.length()];
+
+	
+	// Add the arrays
+
+	return 0;
+}
